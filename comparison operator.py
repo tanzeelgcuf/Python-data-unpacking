26d9a0619d324b4240423ef7b99b382b536b1471
tanzeelgcuf <47454751+tanzeelgcuf@users.noreply.github.com>
2019-04-08 14:10:50 +0500

    Add files via upload

---
+++ comparison operator.py
@@ -0,0 +1,16 @@
+name= "amaaar haider"
+print(len(name))
+if len(name)<3:
+    print("name must  be 3 characters long")
+elif len(name)>50:
+    print("name must be less than 50 characters")
+elif len(name)>3 and len(name)<50:
+    print("name is good")
+
+else:
+ print("enjoy")
+
+
+
+
+
